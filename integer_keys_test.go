5e96004073b80a492b6f315f94ce1180ad8e5c4e
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-08-08 12:33:44 +0300

    Added tests for IncrInt/DecrInt methods

---
+++ integer_keys_test.go
@@ -0,0 +1,35 @@
+package raphanus
+
+import "testing"
+
+func Test_IncrInt(t *testing.T) {
+	raph := New()
+
+	raph.SetInt("key", 7)
+	if err := raph.IncrInt("key_fake"); err == nil {
+		t.Errorf("IncrInt got error failed")
+	}
+	if err := raph.IncrInt("key"); err != nil {
+		t.Errorf("IncrInt got error: %v", err)
+	}
+
+	if v, err := raph.GetInt("key"); err != nil || v != 8 {
+		t.Error("IncrInt failed")
+	}
+}
+
+func Test_DecrInt(t *testing.T) {
+	raph := New()
+
+	raph.SetInt("key", 7)
+	if err := raph.DecrInt("key_fake"); err == nil {
+		t.Errorf("DecrInt got error failed")
+	}
+	if err := raph.DecrInt("key"); err != nil {
+		t.Errorf("DecrInt got error: %v", err)
+	}
+
+	if v, err := raph.GetInt("key"); err != nil || v != 6 {
+		t.Error("DecrInt failed")
+	}
+}
