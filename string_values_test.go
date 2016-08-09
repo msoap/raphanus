12e765cc54d445539b327fbe56850e74fbf71712
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-08-09 15:25:17 +0300

    Added tests for string values

---
+++ string_values_test.go
@@ -0,0 +1,32 @@
+package raphanus
+
+import "testing"
+
+func Test_StringMethods(t *testing.T) {
+	raph := New()
+
+	raph.SetStr("key", "value", 0)
+	vStr, err := raph.GetStr("key")
+	if err == ErrKeyNotExists {
+		t.Error("Got ErrKeyNotExists error")
+	}
+
+	if vStr != "value" {
+		t.Errorf("GetStr:\ngot:      %s\nexpected: %s", vStr, "value")
+	}
+
+	vStr, err = raph.GetStr("key_fake")
+	if err != ErrKeyNotExists {
+		t.Error("Not got ErrKeyNotExists error")
+	}
+
+	err = raph.UpdateStr("key", "new value")
+	if err != nil {
+		t.Errorf("UpdateStr got error: %v", err)
+	}
+
+	err = raph.UpdateStr("key_fake", "new value")
+	if err != ErrKeyNotExists {
+		t.Error("Not got ErrKeyNotExists error")
+	}
+}
